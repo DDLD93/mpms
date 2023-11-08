32f04d4f27772a2528c86b5d003b29d9e3e92e9d
ddld93 <umar.jere@gmail.com>
2023-11-08 15:15:26 +0100

    mongo db

--- project/src/config.js
+++ project/src/config.js
@@ -9,7 +9,7 @@ module.exports = {
     app: {
         port: APP_PORT || 3000,
         domain: APP_DOMAIN || "http://localhost:3000",
-        mongoUri: APP_MONGO_URI || "mongodb://localhost:27017/mpms"
+        mongoUri: APP_MONGO_URI || "mongodb+srv://ddld93:1234567890@cluster0.fljiocn.mongodb.net"
     },
     schedulerURL: SCHEDULER_URL || "http://localhost:9000/api/v1/scheduler/add"
 
